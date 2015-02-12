6b5c374cddcc5ab91f00343bffa5a784cbe13630
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2015-02-12 14:09:41 +0000

    Kinesis streamer

---
+++ README.md
@@ -0,0 +1,1 @@
+# node-aws-kinesis-stream
---
+++ kinesisStream.js
@@ -0,0 +1,82 @@
+var aws = require ( 'aws-sdk' );
+var R = require ( 'ramda' );
+var hl = require ( 'highland' );
+
+var processResult = function ( process, push ) {
+    var pushFuncs = {
+        error: push,
+        success: R.lPartial ( push, null ),
+        successList: R.forEach ( R.lPartial ( push, null ) )
+    };
+
+    return function ( error, result ) {
+        if ( error ) {
+            pushFuncs.error ( error );
+            pushFuncs.success ( hl.nil );
+        } else {
+            process ( pushFuncs, result );
+        }
+    };
+};
+
+var wrapKinesisFunction = function ( kinesis, func, parms ) {
+    return function ( push, next ) {
+        kinesis[func] ( parms, processResult ( function ( pushFuncs, result ) {
+            pushFuncs.success ( result );
+            pushFuncs.success ( hl.nil );
+        }, push ) );
+    };
+};
+
+var getNextIteration = function ( kinesis, nextShardIterator, timeout ) {
+    return hl ( function ( push, next ) {
+        kinesis.getRecords ( { ShardIterator: nextShardIterator, Limit: 1000 }, processResult ( function ( pushFuncs, result ) {
+            if ( R.type ( result.Records ) !== 'Array' ) {
+                pushFuncs.error ( 'Results is not an array' );
+            } else {
+                if ( R.isEmpty ( result.Records ) ) {
+                    setTimeout ( function () {
+                        /* What to do if result.NextShardIterator is undefined? */
+                        next ( getNextIteration ( kinesis, result.NextShardIterator, R.min ( [ timeout + 100, 1000 ] ) ) );
+                    }, timeout );
+                } else {
+                    pushFuncs.successList ( result.Records );
+                    next ( getNextIteration ( kinesis, result.NextShardIterator, 0 ) );
+                }
+            }
+        }, push ) );
+    } );
+};
+
+module.exports = R.curry ( function ( region, streamName ) {
+    var kinesis = new aws.Kinesis ( { region: region } );
+    var kinesisConfig = R.mixin ( R.createMapEntry ( 'StreamName', streamName ), R.createMapEntry ( 'ShardIteratorType', 'LATEST' ) );
+
+    return hl ( wrapKinesisFunction ( kinesis, 'describeStream', R.omit ( [ 'ShardIteratorType' ], kinesisConfig ) ) )
+        .pluck ( 'StreamDescription' )
+        .pluck ( 'Shards' )
+        .map ( R.pluck ( 'ShardId' ) )
+        .flatMap ( function ( shardIdArray ) {
+            return hl ( shardIdArray )
+                .map ( R.createMapEntry ( 'ShardId' ) )
+                .map ( R.mixin ( kinesisConfig ) )
+                .map ( function ( shardConfig ) {
+                    return hl ( wrapKinesisFunction ( kinesis, 'getShardIterator', shardConfig ) ).flatMap ( function ( result ) {
+                        return getNextIteration ( kinesis, result.ShardIterator, 0 );
+                    } );
+                } )
+                .parallel ( R.length ( shardIdArray ) );
+        } )
+        .map ( function ( record ) {
+            return record.Data.toString ();
+        } )
+        .flatMap ( function ( record ) {
+            return hl.wrapCallback ( function ( record, callBack ) {
+                try {
+                    return callBack ( null, JSON.parse ( record ) );
+                } catch ( error ) {
+                    return callBack ( error );
+                }
+            } )( record );
+        } );
+} );
